32973b3ca301e12928d48cfd8a4712139266d7c2
Kim Zugic <kim.zugic@live.com>
2024-11-17 21:20:49 +0100

    Dialog box to enter name when connecting

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -2,6 +2,7 @@ package Client;
 
 import QuizApp.QuizAppFrame;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,6 +17,7 @@ public class Client {
     private final int PORT = 6000;
     QuizAppFrame frame = new QuizAppFrame();
 
+
     public Client() throws UnknownHostException {
         initializingConnection();
     }
@@ -29,7 +31,10 @@ public class Client {
                  BufferedReader serverReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
                 System.out.println("Streams created, connection established.");
+                frame.setPlayer1Name(JOptionPane.showInputDialog(null,"Enter your name"));
+                frame.setPlayer2Name(JOptionPane.showInputDialog(null,"Enter your name"));
                 frame.mainFrame();
+
                 String fromUser;
                 while ((fromUser = userReader.readLine()) != null) {
                     serverWriter.println(fromUser);
