4854314c66706dcf9f13bda09d590655adbfa155
Linn Edvardsson <linn.edvardsson@yh.nackademin.se>
2024-11-20 14:57:54 +0100

    building connection between client/server/gui

--- src/server/ClientConnection.java
+++ src/server/ClientConnection.java
@@ -10,6 +10,8 @@ public class ClientConnection implements Runnable {
     private final InetAddress ip = InetAddress.getLocalHost();
     private final int PORT = 6000;
     private Socket socket;
+    ObjectOutputStream out;
+    ObjectInputStream in;
 
     public ClientConnection(Socket socket) throws UnknownHostException {
         this.socket = socket;
@@ -18,13 +20,31 @@ public class ClientConnection implements Runnable {
     @Override
     public void run() {
         try {
-            initializingConnection();
+            initializingConnection2();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private void initializingConnection() throws IOException {
+    private void initializingConnection2() throws IOException {
+        try {
+            out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
+
+            System.out.println("Connected to server");
+            Object objIn;
+            while ((objIn = in.readObject()) != null) {
+                if (objIn instanceof Object obj){
+                    System.out.println("Received object from client");
+                    out.writeObject(new Object());
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*private void initializingConnection() throws IOException {
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
@@ -34,6 +54,6 @@ public class ClientConnection implements Runnable {
             out.println("u wrote: " + fromClient);
             System.out.println(fromClient);
         }
-    }
+    }*/
 
 }
